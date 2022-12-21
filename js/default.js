2c7fe064154d99c3be6d500493d724944c0f8130
Dylan Jansen <dylanjansen1999@hotmail.com>
2022-12-21 22:27:43 +0100

    add files

---
+++ js/default.js
@@ -0,0 +1,31 @@
+window.onload = async function() {
+    // await getRepoFile()
+    // menuButtonEvent();
+};
+
+//menubutton click event 
+function menuButtonEvent(){
+    const menuButton = document.querySelector(".nav-bar-button")
+    const menuContent = document.querySelector(".nav-bar-container")
+    let onOff = false;
+    menuButton.addEventListener('click', (event) => {
+        if(onOff === true){
+            onOff = false
+            menuButton.textContent = "sort"
+            menuContent.style.display = "none"
+        } else {
+            onOff = true
+            menuButton.textContent = "close"
+            menuContent.style.display = "block"
+        }
+    });
+}
+
+
+async function getRepoFile(){
+    await $.get("../views/navbarView.html", function(data) {
+        console.log(data)
+        document.querySelector(".test").innerHTML = data
+    });
+}
+
